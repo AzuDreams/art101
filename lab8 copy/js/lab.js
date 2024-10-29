e514e53c2923829ff13d677d43d7e74235847d20
AzuDreams <bensilver2003@gmail.com>
2024-10-28 21:54:31 -0700

    Lab 7

--- lab8 copy/js/lab.js
+++ lab8 copy/js/lab.js
@@ -7,15 +7,13 @@
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
-function makeItRainbow() {
-  var userIntput = window.prompt("Input text to be displayed. Try to input enought text to reveal the image!");
-  userIntput = userIntput.toUpperCase();
-  document.writeln("<h5 class=\"rainbow\">" + userIntput + "</h5>");
+function myFunction(param1, param2) {
+  // some code here
+  // return results;
 }
 
 function main() {
   console.log("Main function started.");
-  makeItRainbow();
   // the code that makes everything happen
 }
 
