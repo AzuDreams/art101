54f6f2ee92f3dcb0d4a76e547e0545f3063164c7
AzuDreams <bensilver2003@gmail.com>
2024-11-01 18:37:03 -0700

    lab 8

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,19 +1,30 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - testing Anon Functions and Callbacks
+// Author: Ben Silver
+// Date: 11/1/24
 
 // Constants
+let array = [0,1,2,3,4,5,6,7,8,9,10];
 
 // Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+function factorial(x){
+  if (x == 0 || x == 1){
+      return x;
+  }
+  let total = 1;
+  for (let y = 1; y <= x; y++){
+      total *= y;
+  }
+  return total;
 }
-
 function main() {
-  console.log("Main function started.");
+  console.log(factorial(5));
+  console.log(factorial(17));
+  let mapping = array.map(factorial);
+  console.log(mapping);
+  console.log(array.map(function(x){
+    return x*x;
+  }));
   // the code that makes everything happen
 }
 
