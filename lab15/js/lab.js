c672b8020361d4355c53e27d2af6895c2ede893c
AzuDreams <bensilver2003@gmail.com>
2024-12-05 15:54:37 -0800

    15 and 16

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -3,6 +3,12 @@
 // Date:
 
 // Constants
+const button = document.getElementById('activate');
+const ouptutDiv = document.getElementById('output');
+const pokemonNumber = Math.floor(Math.random() * (1025));
+console.log(pokemonNumber);
+const primUrl = "https://pokeapi.co/api/v2/pokemon/" + pokemonNumber
+console.log(primUrl);
 
 // Functions
 
@@ -13,8 +19,39 @@ function myFunction(param1, param2) {
 }
 
 function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
+  button.addEventListener('click', () => {
+    const pokemonNumber = Math.floor(Math.random() * (1025));
+    console.log(pokemonNumber);
+    const primUrl = "https://pokeapi.co/api/v2/pokemon/" + pokemonNumber
+    console.log(primUrl);
+    $.ajax({
+      // The URL for the request (from the api docs)
+
+      url: primUrl,
+      // The data to send (will be converted to a query string)
+      // Whether this is a POST or GET request
+      type: "GET",
+      // The type of data we expect back
+      dataType : "json",
+      // What do we do when the api call is successful
+      //   all the action goes in here
+      success: function(data) {
+          // do stuff
+          console.log(data);
+          $("#output").html('<div class="text"><p> Pokemon: ' + data.name  + "<br>" +
+          "Ability: " + data.abilities[0].ability.name +
+          '</p></div>');
+          
+
+
+      },
+      // What we do if the api call fails
+      error: function (jqXHR, textStatus, errorThrown) { 
+          // do stuff
+          console.log("Error:", textStatus, errorThrown);
+      }
+  })
+  });
 }
 
 // let's get this party started
--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -3,6 +3,8 @@
 // Date:
 
 // Constants
+const button = document.getElementById('activate');
+const ouptutDiv = document.getElementById('output');
 
 // Functions
 
@@ -13,8 +15,37 @@ function myFunction(param1, param2) {
 }
 
 function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
+  button.addEventListener('click', () => {
+    $.ajax({
+      // The URL for the request (from the api docs)
+      data:{
+
+      },
+
+
+      url: "https://xkcd.com/614/info.0.json",
+      // The data to send (will be converted to a query string)
+      // Whether this is a POST or GET request
+      type: "GET",
+      // The type of data we expect back
+      dataType : "json",
+      // What do we do when the api call is successful
+      //   all the action goes in here
+      success: function(data) {
+          // do stuff
+          $("#output").html('<img src=\"' + data.img + '\">')
+          console.log(data);
+          
+
+
+      },
+      // What we do if the api call fails
+      error: function (jqXHR, textStatus, errorThrown) { 
+          // do stuff
+          console.log("Error:", textStatus, errorThrown);
+      }
+  })
+  });
 }
 
 // let's get this party started
